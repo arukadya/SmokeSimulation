9894327dde1e52c5c82b67336f1d083c956ee32b
arukadya <a19.sj7x@g.chuo-u.ac.jp>
2023-06-13 20:55:52 +0900

    Initial Commit

---
+++ SmokeSimulation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SmokeSimulation
+//
+//  Created by 須之内俊樹 on 2023/06/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
