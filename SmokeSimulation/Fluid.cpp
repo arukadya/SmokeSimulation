828678d54eaccff4df7106ee6c57dc8bec2f3960
arukadya <a19.sj7x@g.chuo-u.ac.jp>
2023-06-17 23:56:33 +0900

    add advect

--- SmokeSimulation/Fluid.cpp
+++ SmokeSimulation/Fluid.cpp
@@ -1,70 +1,158 @@
 //
 //  Fluid.cpp
 
+
+
+
+
+
+
+
+
+
+
+
+//力，密度，温度の初期化(Template Array3<double>)，温度と渦の力計算，計算のループ作成，密度を使ってテキトーな光の計算，平行投影のなんちゃって横断アルゴリズム
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "Fluid.hpp"
 Fluid::Fluid(){}
-Fluid::Fluid(double x,double t,double density){
+//Fluid::Fluid(double x,double t,double density){
+//    dx = x;
+//    dt = t;
+//    rho = density;
+//    L = dx*Nx;
+//    vfi.reset(f0.y());
+//}
+Fluid::Fluid(double x,double t,myArray3d &density,myArray3d &temprature){
     dx = x;
     dt = t;
     rho = density;
-    L = dx*Nx;
-    vfi.reset(f0.y());
+    temp = temprature;
 }
-double Fluid::interporation(double x,double y,double z,myArray3d &val,double dx){
-    int i = fmax(0.0, fmin(val.nx-1-1e-6,x/dx));
-    int j = fmax(0.0, fmin(val.ny-1-1e-6,y/dx));
-    int k = fmax(0.0, fmin(val.nz-1-1e-6,z/dx));
-    
+double Fluid::TriLinearInterporation(double x,double y,double z,myArray3d &val){
+    x = fmax(0.0, fmin(val.nx-1-1e-6,x/dx));
+    y = fmax(0.0, fmin(val.ny-1-1e-6,y/dx));
+    z = fmax(0.0, fmin(val.nz-1-1e-6,z/dx));
+    int i = x;int j = y;int k = z;
+    double s = x-i;double t = y-i;double u = z-i;
+    Eigen::VectorXd f = {
+        val.value[i][j][k],val.value[i+1][j][k],val.value[i][j+1][k],val.value[i][j][k+1],
+        val.value[i+1][j+1][k],val.value[i][j+1][k+1],val.value[i+1][j][k+1],val.value[i+1][j+1][k+1]};
+    Eigen::VectorXd c = {
+        (1-s)*t*u,s*t*u,(1-s)*(1-t)*u,(1-s)*t*(1-u),
+        s*(1-t)*u,(1-s)*(1-t)*(1-u),s*t*(1-u),s*(1-t)*(1-u)};
+    return f.dot(c);
 }
-void Fluid::advect(){
+void Fluid::faceAdvect(){
     //x
     for(int i=1;i<u.nx;++i){
         for(int j=0;j<u.ny;++j){
             for(int k=0;j<u.nz;++k){
-                
+                double x = i*dx;double y = (j+0.5)*dx;double z = (k+0.5)*dx;
+                double adv_x = x - dt*TriLinearInterporation(x, y-0.5*dx, z-0.5*dx, old_u);
+                double adv_y = y - dt*TriLinearInterporation(x-0.5*dx, y, z-0.5*dx, old_v);
+                double adv_z = z - dt*TriLinearInterporation(x-0.5*dx, y-0.5*dx, z, old_w);
+                u.value[i][j][k] = TriLinearInterporation(adv_x, adv_y, adv_z, old_u);
+            }
+        }
+    }
+    for(int i=0;i<v.nx;++i){
+        for(int j=1;j<v.ny;++j){
+            for(int k=0;j<v.nz;++k){
+                double x = (i+0.5)*dx;double y = j*dx;double z = (k+0.5)*dx;
+                double adv_x = x - dt*TriLinearInterporation(x, y-0.5*dx, z-0.5*dx, old_u);
+                double adv_y = y - dt*TriLinearInterporation(x-0.5*dx, y, z-0.5*dx, old_v);
+                double adv_z = z - dt*TriLinearInterporation(x-0.5*dx, y-0.5*dx, z, old_w);
+                v.value[i][j][k] = TriLinearInterporation(adv_x, adv_y, adv_z, old_v);
+            }
+        }
+    }
+    for(int i=0;i<w.nx;++i){
+        for(int j=0;j<w.ny;++j){
+            for(int k=1;j<w.nz;++k){
+                double x = (i+0.5)*dx;double y = (j+0.5)*dx;double z = k*dx;
+                double adv_x = x - dt*TriLinearInterporation(x, y-0.5*dx, z-0.5*dx, old_u);
+                double adv_y = y - dt*TriLinearInterporation(x-0.5*dx, y, z-0.5*dx, old_v);
+                double adv_z = z - dt*TriLinearInterporation(x-0.5*dx, y-0.5*dx, z, old_w);
+                w.value[i][j][k] = TriLinearInterporation(adv_x, adv_y, adv_z, old_w);
             }
         }
     }
 }
-std::vector<int>Fluid::DirichletBoundaryCondition(int i,int j,int k,myMap &map){
-    std::vector<int>ret(6,1);
-    std::vector<std::vector<int>>keys = {{i+1,j,k},{i,j+1,k},{i-1,j,k},{i,j-1,k},{i,j,k-1},{i,j,k+1}};
-    if(i == Nx-1)ret[0] = 0;
-    else if(!map.contains(keys[0]))ret[0] = 0;
-    if(j == Ny-1)ret[1] = 0;
-    else if(!map.contains(keys[1]))ret[1] = 0;
-
-    if(i == 0)ret[2] = 0;
-    else if(!map.contains(keys[2]))ret[2] = 0;
-    if(j == 0)ret[3] = 0;
-    else if(!map.contains(keys[3]))ret[3] = 0;
-
-    if(k == 0)ret[4] = 0;
-    else if(!map.contains(keys[4]))ret[4] = 0;
-    if(k == Nz-1)ret[5] = 0;
-    else if(!map.contains(keys[5]))ret[5] = 0;
-    return ret;
+void Fluid::centerAdvect(myArray3d &val){
+    myArray3d old_val = val;
+    for(int i=0;i<val.nx;++i){
+        for(int j=0;j<val.ny;++j){
+            for(int k=0;j<val.nz;++k){
+                double x = (i+0.5)*dx;double y = (j+0.5)*dx;double z = (k+0.5)*dx;
+                double adv_x = x - dt*TriLinearInterporation(x, y-0.5*dx, z-0.5*dx, u);
+                double adv_y = y - dt*TriLinearInterporation(x-0.5*dx, y, z-0.5*dx, v);
+                double adv_z = z - dt*TriLinearInterporation(x-0.5*dx, y-0.5*dx, z, w);
+                val.value[i][j][k] = TriLinearInterporation(adv_x, adv_y, adv_z, old_val);
+            }
+        }
+    }
 }
-void Fluid::project(myMap &map){
+
+//std::vector<int>Fluid::DirichletBoundaryCondition(int i,int j,int k,myMap &map){
+//    std::vector<int>ret(6,1);
+//    std::vector<std::vector<int>>keys = {{i+1,j,k},{i,j+1,k},{i-1,j,k},{i,j-1,k},{i,j,k-1},{i,j,k+1}};
+//    if(i == Nx-1)ret[0] = 0;
+//    else if(!map.contains(keys[0]))ret[0] = 0;
+//    if(j == Ny-1)ret[1] = 0;
+//    else if(!map.contains(keys[1]))ret[1] = 0;
+//
+//    if(i == 0)ret[2] = 0;
+//    else if(!map.contains(keys[2]))ret[2] = 0;
+//    if(j == 0)ret[3] = 0;
+//    else if(!map.contains(keys[3]))ret[3] = 0;
+//
+//    if(k == 0)ret[4] = 0;
+//    else if(!map.contains(keys[4]))ret[4] = 0;
+//    if(k == Nz-1)ret[5] = 0;
+//    else if(!map.contains(keys[5]))ret[5] = 0;
+//    return ret;
+//}
+//void Fluid::project(myMap &map){
+void Fluid::project(){
     SparseMatrix A(Nx*Ny*Nz,Nx*Ny*Nz),B(Nx*Ny*Nz,Nx*Ny*Nz);
     Eigen::VectorXd b = Eigen::VectorXd::Zero(Nx*Ny*Nz);
     Eigen::VectorXd px;
-    std::set<int> DirichletKey;
-    std::vector<std::vector<int>>keys;
+//    std::set<int> DirichletKey;
+//    std::vector<std::vector<int>>keys;
     //Tripletの計算
     std::vector<Triplet> triplets;
     for(int i=0;i<Nx;i++){
         for(int j=0;j<Ny;j++){
             for(int k=0;k<Nz;k++){
-                std::vector<int>key = {i,j,k};
-                if(!map.contains(key)){
-                    //前処理でAが変更されてしまうので，境界条件として別で無理矢理設定する．
-                    triplets.emplace_back(i+j*Nx+k*Nx*Ny,i+j*Nx+k*Nx*Ny,1);
-                    keys.push_back(key);
-                    DirichletKey.insert(i+j*Nx+k*Nx*Ny);
-                    continue;
-                }
-                double scale = dt/(rho*dx*dx);
+//                std::vector<int>key = {i,j,k};
+//                if(!map.contains(key)){
+//                    //前処理でAが変更されてしまうので，境界条件として別で無理矢理設定する．
+//                    triplets.emplace_back(i+j*Nx+k*Nx*Ny,i+j*Nx+k*Nx*Ny,1);
+//                    keys.push_back(key);
+//                    DirichletKey.insert(i+j*Nx+k*Nx*Ny);
+//                    continue;
+//                }
+                double scale = dt/(rho.value[i][j][k]*dx*dx);
                 //std::cout << i << "," << j << std::endl;
                 double D[6] = {1.0,1.0,-1.0,-1.0,-1.0,1.0};//周囲6方向に向かって働く、圧力の向き
                 //double F[4] = {(double)(i<Nx-1),(double)(j<Ny-1),(double)(i>0),(double)(j>0)};//境界条件。壁なら0,流体なら1
@@ -83,7 +171,7 @@ void Fluid::project(myMap &map){
                     //sumP += scale;
                     b(i+j*Nx+k*Nx*Ny) += D[n]*F[n]*U[n]/(dx);
                 }
-                F = DirichletBoundaryCondition(i,j,k,map);
+                //F = DirichletBoundaryCondition(i,j,k,map);
 //                    for(int n=0;n<6;n++){
 //                        std::cout << F_pri[n] << "," << F[n] << std::endl;
 //                    }
@@ -99,14 +187,14 @@ void Fluid::project(myMap &map){
     }
     A.setFromTriplets(triplets.begin(), triplets.end());
     Eigen::ConjugateGradient<SparseMatrix> solver;
-    for(int i=0;i<A.outerSize();++i){
-        for(SparseMatrix::InnerIterator it(A,i);it;++it){
-            if(it.row() == *DirichletKey.begin()){
-                it.valueRef() = 1;
-                DirichletKey.erase(DirichletKey.begin());
-            }
-        }
-    }
+//    for(int i=0;i<A.outerSize();++i){
+//        for(SparseMatrix::InnerIterator it(A,i);it;++it){
+//            if(it.row() == *DirichletKey.begin()){
+//                it.valueRef() = 1;
+//                DirichletKey.erase(DirichletKey.begin());
+//            }
+//        }
+//    }
     solver.compute(A);
     px = solver.solve(b);
     for(int i=0;i<Nx;i++){
@@ -118,18 +206,27 @@ void Fluid::project(myMap &map){
     }
     for(int i=1; i<Nx;i++){
         for(int j=0;j<Ny;j++){
-            for(int k=0;k<Nz;k++)u.value[i][j][k] = u.value[i][j][k] - dt/rho * (p.value[i][j][k]-p.value[i-1][j][k])/dx;
+            for(int k=0;k<Nz;k++)u.value[i][j][k] = u.value[i][j][k] - dt/rho.value[i][j][k] * (p.value[i][j][k]-p.value[i-1][j][k])/dx;
         }
     }
     for(int i=0;i<Nx;i++){
         for(int j=1;j<Ny;j++){
-            for(int k=0;k<Nz;k++)v.value[i][j][k] = v.value[i][j][k] - dt/rho * (p.value[i][j][k]-p.value[i][j-1][k])/dx;
+            for(int k=0;k<Nz;k++)v.value[i][j][k] = v.value[i][j][k] - dt/rho.value[i][j][k] * (p.value[i][j][k]-p.value[i][j-1][k])/dx;
         }
     }
     for(int i=0;i<Nx;i++){
         for(int j=0;j<Ny;j++){
-            for(int k=1;k<Nz;k++)w.value[i][j][k] = w.value[i][j][k] - dt/rho * (p.value[i][j][k]-p.value[i][j][k-1])/dx;
+            for(int k=1;k<Nz;k++)w.value[i][j][k] = w.value[i][j][k] - dt/rho.value[i][j][k] * (p.value[i][j][k]-p.value[i][j][k-1])/dx;
         }
     }
 }
-
+void cal_buoyanacy(){
+    Eigen::Vector3d dir_gravity = {0.0,1.0,0.0};
+    for(int i=0;i<Nx;i++){
+        for(int j=0;j<Ny;j++){
+            for(int k=0;k<Nz;k++){
+                
+            }
+        }
+    }
+}
--- SmokeSimulation/Fluid.hpp
+++ SmokeSimulation/Fluid.hpp
@@ -22,6 +22,7 @@
 #define Ny 16
 #define Nz 16//グリッドの数
 
+#define Tamb 25
 #define g0 9.8
 using ScalarType = double;
 using IndexType = int64_t;
@@ -31,31 +32,27 @@ typedef Eigen::SparseMatrix<double, Eigen::RowMajor, int64_t> SpMat;
 struct Fluid{
     double dx;//セルの大きさ
     double dt;//時間の刻み幅
-    double rho;
+    //double rho;
     myArray3d u = myArray3d(Nx+1,Ny,Nz,0);//水平
     myArray3d old_u = myArray3d(Nx+1,Ny,Nz,0);//水平
-    myArray3d umi = myArray3d(Nx+1,Ny,Nz,0);//Gridの重さ
-    myArray3d ufi = myArray3d(Nx+1,Ny,Nz,0);//Gridに加わる外力
-    
     myArray3d v = myArray3d(Nx,Ny+1,Nz,0);//鉛直
     myArray3d old_v = myArray3d(Nx,Ny+1,Nz,0);//水平
-    myArray3d vmi = myArray3d(Nx,Ny+1,Nz,0);//Gridの重さ
-    myArray3d vfi = myArray3d(Nx,Ny+1,Nz,0);//Gridに加わる外力
-    
     myArray3d w = myArray3d(Nx,Ny,Nz+1,0);
     myArray3d old_w = myArray3d(Nx,Ny,Nz+1,0);
-    myArray3d wmi = myArray3d(Nx,Ny,Nz+1,0);//Gridの重さ
-    myArray3d wfi = myArray3d(Nx,Ny,Nz+1,0);//Gridに加わる外力
-    
     myArray3d p = myArray3d(Nx,Ny,Nz,0);//圧力
-    std::vector<double> weights;//粒子の重み
-    double L;
-    Eigen::Vector3d f0 = {0.0,-g0,0.0};
+    myArray3d rho = myArray3d(Nx,Ny,Nz,0);
+    myArray3d temp = myArray3d(Nx,Ny,Nz,Tamb);
+    
+    //double L;
     Fluid();
-    Fluid(double x,double t,double density);
-    std::vector<int>DirichletBoundaryCondition(int i,int j,int k,myMap &map);
-    void project(myMap &map);
-    void advect();
-    double Fluid::interporation(double x,double y,double z,myArray3d &val,double dx);
+    //Fluid(double x,double t,double density);
+    Fluid(double x,double t,myArray3d &density,myArray3d &temprature);
+    //std::vector<int>DirichletBoundaryCondition(int i,int j,int k,myMap &map);
+    void project();
+//    void project(myMap &map);
+    void faceAdvect();
+    void centerAdvect(myArray3d &val);
+    double TriLinearInterporation(double x,double y,double z,myArray3d &val);
+    
 };
 #endif /* Fluid_hpp */
